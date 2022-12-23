347d54c79eb76a5ea680e2d7d3030704906f7356
jbmagination <26027089+jbmagination@users.noreply.github.com>
2022-12-23 17:43:19 -0500

    Custom images, suffix options, and link fixes

--- index.js
+++ index.js
@@ -27,14 +27,18 @@ app.post('/getData', (req, res) => {
             if (shortLink.startsWith('exoracer.page.link/')) shortLink = shortLink.substring(19)
 
             let additionalCharacter;
+            let suffixOption;
             if (shortLink.indexOf('?') > -1) {
-                additionalCharacter = '&'
+                additionalCharacter = '&';
+                suffixOption = "LONG";
             } else {
                 additionalCharacter = '?'
                 switch (shortLink.length) {
                     case 4:
+                        suffixOption = "SHORT";
                         break;
                     case 17:
+                        suffixOption = "UNGUESSABLE";
                         break;
                     default:
                         res.json({
@@ -47,7 +51,7 @@ app.post('/getData', (req, res) => {
             }
 
             // actually doing the thing
-            console.log(`Loading flowchart for exoracer.page.link/${shortLink}`)
+            console.log(`Loading preview link for exoracer.page.link/${shortLink}`)
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             
@@ -56,7 +60,7 @@ app.post('/getData', (req, res) => {
             });
             
             const link = await page.evaluate(() => {
-                return document.getElementsByTagName('a')[0].href.baseVal;
+                return document.getElementsByClassName('cacJ4e')[0].getElementsByTagName('a')[0].href.baseVal;
             });
             if (link) { 
                 console.log('Long dynamic link found'); 
@@ -72,19 +76,20 @@ app.post('/getData', (req, res) => {
             
             await browser.close();
 
-            let levelId = link.substring(link.indexOf('levelId%3D')).substring(10, 46);
+            let levelID = link.substring(link.indexOf('levelId%3D')).substring(10, 46);
             let title = decodeURIComponent(link).substring(decodeURIComponent(link).indexOf('?title=')).substring(7).split('&')[0].replaceAll('+', ' ')
             let description = decodeURIComponent(link).substring(decodeURIComponent(link).indexOf('&description=')).substring(13).split('&')[0].replaceAll('+', ' ')
-            // let title = link.substring(link.indexOf('&st=')).substring(4).split('&')[0].replaceAll('+', ' ')
-            // let description = link.substring(link.indexOf('&sd=')).substring(4).split('&')[0].replaceAll('+', ' ')
+            let imageURL = decodeURIComponent(decodeURIComponent(link).substring(decodeURIComponent(link).indexOf('&imageUrl=')).substring(10).split('&')[0])
             let levelVersion = decodeURIComponent(link).substring(decodeURIComponent(link).indexOf('&levelVersion=')).substring(14).split('&')[0]
 
             res.json({
                 "status": "SUCCESS",
                 "title": title,
                 "description": description,
-                "levelID": levelId,
-                "levelVersion": levelVersion
+                "levelID": levelID,
+                "imageURL": imageURL,
+                "levelVersion": levelVersion,
+                "suffixOption": suffixOption,
             })
         } catch (error) {
             console.log(error)
@@ -100,14 +105,19 @@ app.post('/makeLink', (req, res) => {
     console.log('/makeLink request received');
     (async () => {
         try {
-            // server-side validation + variables
+            // server-side validation and correction + variables
+            let levelID = req.body.levelID;
             let title = req.body.title;
             let description = req.body.description;
-            let levelId = req.body.levelID;
+            let imageURL = req.body.imageURL;
             let levelVersion = parseInt(req.body.levelVersion);
+            let suffixOption;
+
+            if (req.body.suffix == "SHORT") suffixOption = "SHORT"
+            else suffixOption = "UNGUESSABLE"
 
             let levelIDregex = /^[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]$/
-            if (!(levelIDregex.test(levelId))) {
+            if (!(levelIDregex.test(levelID))) {
                 res.json({
                     "status": "ERROR",
                     "error": "Validation error: Invalid level ID"
@@ -118,9 +128,9 @@ app.post('/makeLink', (req, res) => {
             // actually doing the thing
             console.log('Making request to create link')
             await axios.post(`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.LINK_KEY}`, {
-                "longDynamicLink": `https://exoracer.page.link:443/?ibi=com.nyanstudio.exoracer&link=https%3a%2f%2fexoracer.io%2f%3flink%3dLEVEL%26levelId%3d${levelId}%26levelVersion%3d${levelVersion}&si=https%3a%2f%2fstorage.googleapis.com%2fexoracer-bd008.appspot.com%2flevels%2f${levelId}-1.png&sd=${encodeURI(description).replace('%20', '+')}&amv=0&st=${encodeURI(title).replace('%20', '+')}&apn=com.nyanstudio.exoracer&ofl=https%3a%2f%2fexoracer.io%2fdeeplinkfallback.php%3ftitle%3d${encodeURI(title).replace('%20', '%2b')}%26description%3d${encodeURI(description).replace('%20', '%2b')}%26imageUrl%3dhttps%253a%252f%252fstorage.googleapis.com%252fexoracer-bd008.appspot.com%252flevels%252f${levelId}-${levelVersion}.png`,
+                "longDynamicLink": `https://exoracer.page.link:443/?ibi=com.nyanstudio.exoracer&link=https%3a%2f%2fexoracer.io%2f%3flink%3dLEVEL%26levelId%3d${levelID}%26levelVersion%3d${levelVersion}&si=${encodeURIComponent(imageURL)}&sd=${encodeURI(description).replace('%20', '+')}&st=${encodeURI(title).replace('%20', '+')}&apn=com.nyanstudio.exoracer&ofl=https%3a%2f%2fexoracer.io%2fdeeplinkfallback.php%3ftitle%3d${encodeURI(title).replace('%20', '%2b')}%26description%3d${encodeURI(description).replace('%20', '%2b')}%26imageUrl%3d${encodeURIComponent(imageURL)}`,
                 "suffix": {
-                    "option": "UNGUESSABLE"
+                    "option": suffixOption
                 }
             })
             .then((response) => {
