179b813c4678c8ad020378fe8d8a27fb98adc15c
jbmagination <26027089+jbmagination@users.noreply.github.com>
2022-12-28 19:21:06 -0500

    Add client ID to environment variables
    Not harmful to have in the open, but also more aesthetically pleasing to see in the code ^-^

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ let GoogleAuth;
 if (process.env.CUSTOM_LINK == "true") {
     GoogleAuth = new google.auth.GoogleAuth({
         credentials: {
-            "type":"service_account",
-            "project_id":process.env.PROJECT_ID,
-            "private_key_id":process.env.PRIVATE_KEY_ID,
+            "type": "service_account",
+            "project_id": process.env.PROJECT_ID,
+            "private_key_id": process.env.PRIVATE_KEY_ID,
             "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-            "client_email":process.env.CLIENT_EMAIL,
-            "client_id":"115378856528797503755",
-            "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-            "token_uri":"https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url":`https://www.googleapis.com/robot/v1/metadata/x509/${encodeURI(process.env.CLIENT_EMAIL)}`
+            "client_email": process.env.CLIENT_EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURI(process.env.CLIENT_EMAIL)}`
         },
         scopes: [ 'https://www.googleapis.com/auth/firebase' ]
     })
