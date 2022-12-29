a7169ed115264f95a208455121b3ce1e15f1f563
jbmagination <26027089+jbmagination@users.noreply.github.com>
2022-12-28 19:17:16 -0500

    Move account object from `keyFile` to `credentials`
    This commit also removes GLITCH_README.md and removes an unused variable.

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ require('dotenv').config();
 let GoogleAuth;
 if (process.env.CUSTOM_LINK == "true") {
     GoogleAuth = new google.auth.GoogleAuth({
-        keyFile: JSON.parse(process.env.SERVICE_ACCOUNT_JSON),
+        credentials: {
+            "type":"service_account",
+            "project_id":process.env.PROJECT_ID,
+            "private_key_id":process.env.PRIVATE_KEY_ID,
+            "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+            "client_email":process.env.CLIENT_EMAIL,
+            "client_id":"115378856528797503755",
+            "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+            "token_uri":"https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url":`https://www.googleapis.com/robot/v1/metadata/x509/${encodeURI(process.env.CLIENT_EMAIL)}`
+        },
         scopes: [ 'https://www.googleapis.com/auth/firebase' ]
     })
 }
@@ -170,7 +181,6 @@ app.post('/makeLink', (req, res) => {
 
             // actually doing the thing
             console.log('POST /makeLink | Making request to create link')
-            let request;
             if ((suffixOption == "CUSTOM") && (GoogleAuth !== undefined)) {
                 console.log('POST /makeLink | Custom suffix chosen')
                 GoogleAuth.request({
